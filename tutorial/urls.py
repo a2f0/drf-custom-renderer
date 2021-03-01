f365c51c5a06021febcb52b68e17af2ff1a27fad
Dan Sullivan <dansullivan@gmail.com>
2021-03-01 16:37:05 -0500

    get api running

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,3 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from tutorial.quickstart import views
+
 """tutorial URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,8 +19,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
